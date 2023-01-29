1d6bbb297ba1946efcdc0e8cf049bb2d2616d765
“ShubhamvShrikhande” <shubhamvshri99@gmail.com>
2023-01-29 12:48:41 +0530

    Welcome To EmployeeWage

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EmployeeWage
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Employee");
+        }
+    }
+}
